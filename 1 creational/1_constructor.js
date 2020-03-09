edf830d4b4a6863cc5e771ef60c66c127a1e7eca
Sergey Lapitski <s.lapitskij@gmail.com>
2020-03-09 17:31:11 +0300

    learning patterns

--- 1 creational/1_constructor.js
+++ 1 creational/1_constructor.js
@@ -1,12 +1,3 @@
-// function Server(name, ip) {
-//   this.name = name
-//   this.ip = ip
-// }
-//
-// Server.prototype.getUrl = function() {
-//   return `https://${this.ip}:80`
-// }
-
 class Server {
   constructor(name, ip) {
     this.name = name
@@ -18,5 +9,5 @@ class Server {
   }
 }
 
-const aws = new Server('AWS German', '82.21.21.32')
+const aws = new Server('AWS German', '82.21.21.32');
 console.log(aws.getUrl())
